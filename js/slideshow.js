bee19692750514f5cb17d11937928b6b5dd8cd8b
Jon <jonmelnick@hotmail.com>
2017-01-01 17:24:02 -0800

    slideshoq

---
+++ js/slideshow.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const IMAGES = ["Tourmaline-Ring-Angle-View-1-V2", "Tourmaline-Ring-Angle-View-2", "Tourmaline-Ring-Front-View", "Tourmaline-Ring-Thru-Finger-View-1-V2", "Tourmaline-Ring-Thru-Finger-View-2-V2"]
+
+let img = 0;
+
+function changeImage() {
+  increment();
+  let ring = document.getElementById('ring')
+  ring.src = `img/${IMAGES[img]}.jpg`;
+}
+
+function increment(){
+  img = (img + 1)%IMAGES.length
+}
+
+function timer() {
+  setInterval(changeImage, 3000);
+}
+
+function enter(){
+
+}
+
+function leave() {
+
+}
+
+function begin() {
+  timer();
+  let ring = document.getElementById('ring');
+  ring.addEventListener('mouseenter', enter)
+  ring.addEventListener('mouseleave', leave)
+}
